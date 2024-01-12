eed446527a0fc0b1abfc250c5ce16a011266b112
s3818206 <s3818206@rmit.edu.vn>
2024-01-12 13:37:41 +0700

    Fixing little bugs

---
+++ src/main/java/app/PageIndex.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+public class PageIndex implements Handler  {
+    public static final String URL = "/";
+    private static final String TEMPLATE = ("index.html");
+
+    @Override
+    public void handle(Context context) throws Exception {
+        Map<String, Object> model = new HashMap<String, Object>();
+        context.render(TEMPLATE, model);
+    }
+}
